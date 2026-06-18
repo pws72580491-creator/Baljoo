278e156641409182f9eaf1e6f56950c9379f708f
pws72580491-creator <pws72580491@gmail.com>
2026-06-19 07:35:01 +0900

    Add files via upload

---
+++ js/sw.js
@@ -0,0 +1,51 @@
+const CACHE_NAME = '발주관리-cache-v3.2.12';
+const APP_SHELL = [
+  './index.html',
+  './manifest.json',
+  './icon-192.png',
+  './icon-512.png',
+  './icon-512-maskable.png',
+  './js/storage.js',
+  './js/helpers.js',
+  './js/ui.js',
+  './js/modal.js',
+  './js/gemini.js',
+  './js/analyzer.js',
+  './js/delivery.js',
+  './js/manual.js',
+  './js/app.js'
+];
+
+self.addEventListener('install', (e) => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(APP_SHELL))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (e) => {
+  e.waitUntil(
+    caches.keys().then((keys) =>
+      Promise.all(keys.filter((k) => k !== CACHE_NAME).map((k) => caches.delete(k)))
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', (e) => {
+  if (e.request.method !== 'GET') return;
+  e.respondWith(
+    caches.match(e.request).then((cached) => {
+      const fetchPromise = fetch(e.request)
+        .then((networkResp) => {
+          if (networkResp && networkResp.ok) {
+            const respClone = networkResp.clone();
+            caches.open(CACHE_NAME).then((cache) => cache.put(e.request, respClone));
+          }
+          return networkResp;
+        })
+        .catch(() => cached);
+      return cached || fetchPromise;
+    })
+  );
+});
